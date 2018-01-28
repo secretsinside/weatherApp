05291d2e599d9c0946d6d19a95b5f43de62b25c2
secretsinside <07nith.me@gmail.com>
2018-01-29 01:15:38 +0530

    UI improvements

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
 var express= require('express');
+var bodyParser= require('body-parser');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var http = require('http');
 var app= express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('ui'));
 app.use(express.static('image'));
 
-app.get('/',function(req, res){
+app.get('/', function(req, res){
 	res.sendFile(__dirname+'/ui'+'/homePage.html');
 });
 
+app.post('/requestWeather', function(req, result){
+	console.log(req.body.city);
+	//request for weather
+	
+/*
+	http.get("http://api.openweathermap.org/data/2.5/weather?APPID=54564342c25b614aa4eb4005d8011a67&q="+ req.body.city, function(err, res){
+		console.log(res);
+	});
+	
+*/
+	var weather = new XMLHttpRequest();
+	weather.open("GET","http://api.openweathermap.org/data/2.5/weather?APPID=54564342c25b614aa4eb4005d8011a67&q="+req.body.city 	,false);
+	weather.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	weather.send(null);
+	console.log(weather.response);
+	
+});
 
 var lisetener= app.listen(8008);
 console.log("server running on port %s", lisetener.address().port);
--- ui/home.js
+++ ui/home.js
@@ -13,15 +13,20 @@ function triggerButton(event){
 }
 
 function fetchData(){
-	var form=document.getElementById("city");
-	var city="";
-	city=form.elements[0].value;
 	
+	var city= document.getElementById("city").value;
+	/*
+	var weather= new XMLHttpRequest();
+	weather.open("POST", "/requestWeather", true);
+	weather.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	weather.send(city);
+	*/
+
 	//store APPID in variable
 	var weather = new XMLHttpRequest();
-	weather.open("GET","http://api.openweathermap.org/data/2.5/weather?APPID=54564342c25b614aa4eb4005d8011a67&q="+city,false);
+	weather.open("GET","http://api.openweathermap.org/data/2.5/weather?APPID=54564342c25b614aa4eb4005d8011a67&q="+city 	,false);
 	weather.send(null);
-
+	
 
 	var weatherResponse = JSON.parse(weather.response);
 	var tempResponse = (parseInt(weatherResponse.main.temp)-273.15).toFixed(2);
@@ -29,11 +34,10 @@ function fetchData(){
 	document.getElementById("returnTemp").innerHTML=tempResponse.toString()+" &#8451";
 	document.getElementById("returnCity").innerHTML=city.toUpperCase();
 	//&#8451 is for the symbol degree
-	if(parseInt(tempResponse)<20){
-		document.body.style.backgroundImage = "url('/windySnow.jpg')";
-	}
-	else{
-		document.body.style.backgroundImage = "url('/skySunny.jpg')";
-	}
+	document.getElementById("city").value= "";
+
+	var history= document.getElementById('history').innerHTML;
+	history+=  '<div class="chip">'+ city+ '<span class="closebtn" onclick="this.parentElement.style.display=\'none\'">&times;</span></div>';
+	document.getElementById('history').innerHTML= history;
 }
 
